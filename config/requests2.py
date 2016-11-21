08094efcf48cab8f31ac3b5ffd4ff001cfaa3d25
Sagar Shah <sagarcs1991@gmail.com>
2016-11-20 22:05:54 -0500

    Add latestVersionBefore, cachedUpdates and some code to read requests and print it

--- config/requests2.py
+++ config/requests2.py
@@ -2,7 +2,9 @@ requests = {
         1:
             {
                 "subjid": 1,
+                "subjtype": "employee",
                 "resid" : 257,
+                "restype": "bank",
                 "action": "read",
                 "wait"  : 6
             }
@@ -10,14 +12,18 @@ requests = {
         2:
             {
                 "subjid": 2,
+                "subjtype": "employee",
                 "resid" : 257,
+                "restype": "bank",
                 "action": "read",
             }
         ,
         3:
             {
                 "subjid": 1,
+                "subjtype": "employee",
                 "resid" : 258,
+                "restype": "bank",
                 "action": "read",
                 "wait"  : 1
             }
@@ -25,21 +31,27 @@ requests = {
         4:
             {
                 "subjid": 1,
+                "subjtype": "employee",
                 "resid" : 257,
+                "restype": "bank",
                 "action": "read"
             }
         ,
         5:
             {
                 "subjid": 2,
+                "subjtype": "employee",
                 "resid" : 258,
+                "restype": "bank",
                 "action": "read"
             }
         ,
         6:
             {
                 "subjid": 3,
+                "subjtype": "employee",
                 "resid" : 257,
+                "restype": "bank",
                 "action": "read"
             }
 }
